4cfe8214b172c78be3956fc7a5af919f88207e7c
Artyom <132067879+TyomBen@users.noreply.github.com>
2023-08-04 15:22:31 +0400

    little changes

--- country.php
+++ country.php
@@ -1,8 +1,8 @@
 <?php
 require ('config.php');
 $value = $_GET['param'];
-   $sql1 = "SELECT name FROM country WHERE continent = '" . $value . "' ";
-   $result = $conn -> query($sql1);
+   $sql = "SELECT name FROM country WHERE continent = '" . $value . "' ";
+   $result = $conn -> query($sql);
    if ($result -> rowCount() > 0){
     $finish = $result -> fetchAll (PDO::FETCH_ASSOC);
     echo json_encode ($finish);
